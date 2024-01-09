6ce308597ba72cf6d9fe648c5462439c2ca663c0
Danny van Kooten <dannyvankooten@users.noreply.github.com>
2024-01-09 15:18:48 +0100

    add support for footnotes

--- gozer.go
+++ gozer.go
@@ -6,10 +6,6 @@ import (
 	"encoding/xml"
 	"flag"
 	"fmt"
-	"github.com/BurntSushi/toml"
-	"github.com/yuin/goldmark"
-	"github.com/yuin/goldmark/extension"
-	"github.com/yuin/goldmark/renderer/html"
 	"html/template"
 	"io/fs"
 	"net/http"
@@ -19,13 +15,18 @@ import (
 	"strings"
 	"sync"
 	"time"
+
+	"github.com/BurntSushi/toml"
+	"github.com/yuin/goldmark"
+	"github.com/yuin/goldmark/extension"
+	"github.com/yuin/goldmark/renderer/html"
 )
 
 var md = goldmark.New(
 	goldmark.WithRendererOptions(
 		html.WithUnsafe(),
 	),
-	goldmark.WithExtensions(extension.GFM))
+	goldmark.WithExtensions(extension.GFM, extension.Footnote))
 
 var frontMatter = []byte("+++")
 
